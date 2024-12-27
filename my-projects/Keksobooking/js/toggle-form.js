4cd4c7584e6dd00fdd33d8395fb923057ec60a79
Владислав <vlados66625@mail.ru>
2024-12-27 13:38:38 +0300

    добавляет big-trip

--- my-projects/Keksobooking/js/toggle-form.js
+++
@@ -1,9 +0,0 @@
-const toggleForm = (form, isDisabled) => {
-  form.classList.toggle('ad-form--disabled', isDisabled);
-  const formElements = form.querySelectorAll('fieldset, select');
-  formElements.forEach((element) => {
-    element.disabled = isDisabled;
-  });
-};
-
-export { toggleForm };
